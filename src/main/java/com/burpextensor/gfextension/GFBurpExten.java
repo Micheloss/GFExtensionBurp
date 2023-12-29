b4b7e7285fdce2500433c5b0f77d3a9f4fc15099
Micheloss <miguelangeldiazbautista@gmail.com>
2023-12-29 09:54:20 +0100

    Version 0.2

--- src/main/java/com/burpextensor/gfextension/GFBurpExten.java
+++ src/main/java/com/burpextensor/gfextension/GFBurpExten.java
@@ -16,9 +16,18 @@ import com.google.gson.reflect.TypeToken;
 public class GFBurpExten implements IBurpExtender, IHttpListener {
     private IBurpExtenderCallbacks callbacks;
     private IExtensionHelpers helpers;
-    private Map<String, List<String>> regexPatterns = new HashMap<>();
+    private HashMap<String, List<String>> regexPatterns = new HashMap<>();
 	private IExtensionHelpers callbacksHelpers;
 
+	
+	private ArrayList<String> wellKnownHeaders;
+	
+	private HashMap<String, String> alreadyReportedParameters;
+	private HashMap<String, String> alreadyReportedHeaders;
+	private HashMap<String, String> alreadyReportedCookies;
+
+	
+	
     @Override
     public void registerExtenderCallbacks(IBurpExtenderCallbacks callbacks) {
         this.callbacks = callbacks;
@@ -27,13 +36,18 @@ public class GFBurpExten implements IBurpExtender, IHttpListener {
         this.helpers = callbacks.getHelpers();
         callbacks.setExtensionName("GF Regex Pattern Detector");
         callbacks.registerHttpListener(this);
-
+        
+        alreadyReportedParameters = new HashMap<String, String>();
+        alreadyReportedHeaders = new HashMap<String, String>();
+        alreadyReportedCookies = new HashMap<String, String>();
         // Load regex patterns from JSON files
         loadPatterns();
     }
 
     private void loadPatterns() {
-        String[] patternFiles = {"debug_logic.json", "idor.json",  "interestingparams.json", "lfi.json", "rce.json", "redirect.json", "sqli.json", "ssrf.json", "ssti.json", "xss.json"};
+        
+    	// Parameters
+    	String[] patternFiles = {"debug_logic.json", "idor.json",  "interestingparams.json", "lfi.json", "rce.json", "redirect.json", "sqli.json", "ssrf.json", "ssti.json", "xss.json"};
         Gson gson = new Gson();
         for (String file : patternFiles) {
             try {
@@ -43,19 +57,11 @@ public class GFBurpExten implements IBurpExtender, IHttpListener {
                 InputStreamReader reader = new InputStreamReader(inputStream);
                 
                 JsonPattern jsonPatterns = gson.fromJson(reader, JsonPattern.class);
-
-/*
-                InputStream in = GFBurpExten.class.getClassLoader().getResourceAsStream(file);
-                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-                
-                JsonPattern jsonPatterns = gson.fromJson(reader, JsonPattern.class);
-*/
-                
+          
                 List<String> patterns = jsonPatterns.getPatterns();
 
                 for (String pattern : patterns) {
                     callbacks.printOutput("Proccessed " + pattern + "");
-                    //callbacks.printOutput("Proccessed " + pattern + " from " + file.split(".")[0]);
 
                 }
                 
@@ -65,42 +71,101 @@ public class GFBurpExten implements IBurpExtender, IHttpListener {
                 callbacks.printError("Error loading pattern file " + file + ": " + e.getMessage());
             }
         }
+        
+        //Headers
+        wellKnownHeaders = new ArrayList<String>();
+        ClassLoader classLoader = getClass().getClassLoader();
+        try (InputStream inputStream = classLoader.getResourceAsStream("headers.json");
+                InputStreamReader isr = new InputStreamReader(inputStream);
+                BufferedReader reader = new BufferedReader(isr)) {
+
+               String line;
+               while ((line = reader.readLine()) != null) {
+            	   wellKnownHeaders.add(line.toLowerCase());
+               }
+
+           } catch (IOException e) {
+               e.printStackTrace();
+           }
     }
 
     @Override
     public void processHttpMessage(int toolFlag, boolean messageIsRequest, IHttpRequestResponse messageInfo) {
-        if (messageIsRequest) {
+        
+    	if (! (toolFlag == IBurpExtenderCallbacks.TOOL_PROXY)) {
+            return;
+        }
+    	
+    	if (messageIsRequest) {
             // Extract and analyze request parameters
             IRequestInfo requestInfo = helpers.analyzeRequest(messageInfo);
             List<IParameter> parameters = requestInfo.getParameters();
             for (IParameter parameter : parameters) {
                 checkPattern(parameter, messageInfo);
             }
+            
+            //Process headers
+            checkHeaders(messageInfo);
+            
         }
     }
 
+    private void checkHeaders(IHttpRequestResponse messageInfo) {
+    	
+        List<String> headers = helpers.analyzeRequest(messageInfo).getHeaders();
+        
+    	for (String header : headers) {
+            String[] parts = header.split(":", 2);
+            if (parts.length == 2) {
+            	
+                String headerName = parts[0].trim();
+                String headerValue = parts[1].trim();
+                
+                
+                //callbacks.printOutput("Header Name: " + headerName + ", Header Value: " + headerValue);
+                
+                if(! headerName.toLowerCase().startsWith("cookie:") && !(wellKnownHeaders.contains(headerName.toLowerCase()))) {
+                
+                    printIssue(messageInfo, headerName, "header", "header");
+                	
+                }
+              
+            }
+        }
+    }
+    
+    
     private void checkPattern(IParameter parameter, IHttpRequestResponse messageInfo) {
+    	
         for (String patternKey : regexPatterns.keySet()) {
             for (String pattern : regexPatterns.get(patternKey)) {
-                //callbacks.printOutput("Found paramenter " +  pattern + " of type "+ parameter.getType());
+
             	if(parameter.getType() != IParameter.PARAM_COOKIE) {
             		 if (parameter.getName().toLowerCase().startsWith(pattern.split("=")[0])) {
-                         //callbacks.issueAlert("Regex pattern match found: " + pattern + " in parameter " + parameter.getName());
-                         callbacks.printOutput("Regex pattern match found: " + pattern + " from" + patternKey +" in parameter " + parameter.getName() + " of type "+ parameter.getType());
-                         printIssue(messageInfo, parameter.getName(), patternKey);
+
+            			 //callbacks.printOutput("Regex pattern match found: " + pattern + " from" + patternKey +" in parameter " + parameter.getName() + " of type "+ parameter.getType());
+                         printIssue(messageInfo, parameter.getName(), patternKey, "parameter");
                      }
+            	} else if (! parameter.getName().startsWith("_")){
+            		//callbacks.printOutput("Regex pattern match found in cookie: " + pattern + " from" + patternKey +" in parameter " + parameter.getName() + " of type "+ parameter.getType());
+                    printIssue(messageInfo, parameter.getName(), patternKey, "cookie");
             	}
                
             }
         }
     }
     
-    private void printIssue(IHttpRequestResponse messageInfo, String parameter, String file) {
+    private void printIssue(IHttpRequestResponse messageInfo, String parameter, String file, String type) {
+    	
+        IScanIssue [] previousIssues = callbacks.getScanIssues(null);
+
+        
+        
     	String url = callbacksHelpers.analyzeRequest(messageInfo).getUrl().toString();
     	if( !(url.contains(".js") || url.contains(".png") || url.contains(".css") || url.contains(".jpg") || url.contains(".jpeg") || url.contains(".js")) ) {
     		
-	    	IHttpRequestResponse[] httpMessages = null; // Mensajes HTTP relacionados con el problema
-	    	IHttpService httpService = messageInfo.getHttpService(); // Servicio HTTP relacionado
+	    	IHttpRequestResponse[] httpMessages = null; 
+	    	IHttpService httpService = messageInfo.getHttpService(); 
 	
 	    	
 	    	byte[] request = messageInfo.getRequest();
@@ -111,23 +176,67 @@ public class GFBurpExten implements IBurpExtender, IHttpListener {
 	    	
 	    	List<int[]> requestHighlights = new ArrayList<>();
 
-	    	if (startOffset != -1) { // Asegúrate de que el string fue encontrado
+	    	if (startOffset != -1) { 
 	    	    requestHighlights.add(new int[] { startOffset, endOffset });
 	    	}
 	    	
-	    	IScanIssue newIssue = new CustomScanIssue(
-	    		callbacksHelpers.analyzeRequest(messageInfo).getUrl(),
-	    	    "[GF] Found problematic parameter",
-	    	    "Low",
-	    	    "Tentative",
-	    	    "Found a parameter that might be problematic. \n Found parameter " + parameter + " . Test for " + file + " attacks",
-	    	    "Test for problem",
-	    	    new IHttpRequestResponse[] { callbacks.applyMarkers(messageInfo, requestHighlights, null)},
-	    	    httpService,
-	    	    "Found a parameter that might be problematic. \n Found parameter " + parameter + " . Test for " + file + " attacks"
-	    	);
+	    	IScanIssue newIssue = null; 
+	    	if(type == "parameter") {
+		    	 newIssue = new CustomScanIssue(
+		    		callbacksHelpers.analyzeRequest(messageInfo).getUrl(),
+		    	    "[GF] Found interesting parameter",
+		    	    "Low",
+		    	    "Tentative",
+		    	    "Found a parameter that might be interesting. \n Found parameter <b> " + parameter + " </b> in "+ callbacksHelpers.analyzeRequest(messageInfo).getUrl().getPath() +" . Test for <b> " + file.split(".json")[0].toUpperCase() + " </b> attacks",
+		    	    "Test for problem",
+		    	    new IHttpRequestResponse[] { callbacks.applyMarkers(messageInfo, requestHighlights, null)},
+		    	    httpService,
+		    	    "Found a parameter that might be interesting. \n Found parameter <b> " + parameter + " </b> "+ callbacksHelpers.analyzeRequest(messageInfo).getUrl().getPath() +" . Test for <b> " + file.split(".json")[0].toUpperCase() + " </b> attacks"
+		    	);
+	    	} else if(type == "cookie") {
+		    	 newIssue = new CustomScanIssue(
+		    		callbacksHelpers.analyzeRequest(messageInfo).getUrl(),
+		    	    "[GF] Found custom cookie parameter",
+		    	    "Low",
+		    	    "Tentative",
+		    	    "Found a cookie parameter that might be interesting. \n Found cookie <b> " + parameter + " </b> .",
+		    	    "Test for problem",
+		    	    new IHttpRequestResponse[] { callbacks.applyMarkers(messageInfo, requestHighlights, null)},
+		    	    httpService,
+		    	    "Found a cookie parameter that might be interesting. \n Found cookie <b> " + parameter + " </b> ."
+		    	);
+	    	} else if(type == "header") {
+		    	 newIssue = new CustomScanIssue(
+		    		callbacksHelpers.analyzeRequest(messageInfo).getUrl(),
+		    	    "[GF] Found custom header",
+		    	    "Low",
+		    	    "Tentative",
+		    	    "Found a custom header that might be interesting. \n Found header <b> " + parameter + " </b> .",
+		    	    "Test for problem",
+		    	    new IHttpRequestResponse[] { callbacks.applyMarkers(messageInfo, requestHighlights, null)},
+		    	    httpService,
+		    	    "Found a custom header that might be interesting. \n Found header <b> " + parameter + " </b> ."
+		    	);
+	    	}
 	
-	    	callbacks.addScanIssue(newIssue);
+	    	
+	    	// Avoid repeated scan issues
+	    	boolean found = false;
+	    	
+	    	for(IScanIssue previousIssue : previousIssues ) {
+	    		
+	    		String previousDetail = previousIssue.getIssueDetail();
+	    		
+	    		if(previousDetail != null && previousDetail.equals(newIssue.getIssueDetail())){
+	    			callbacks.printOutput("Found this issue previously so lets go out");
+	    			found = true;
+	    			break;
+	    		}
+	        }
+	    	
+	    	if(!found) {
+	    		callbacks.addScanIssue(newIssue);
+	    	}
     	}
     }
 }
